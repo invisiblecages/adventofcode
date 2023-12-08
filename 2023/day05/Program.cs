a63f2cc963616609a41085f863d21452b5479c9b
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2023-12-08 18:39:09 +0100

    added day 5 part 2

--- 2023/day05/Program.cs
+++ 2023/day05/Program.cs
@@ -4,31 +4,8 @@ namespace Day05;
 
 internal class Program
 {
-  public static void Solve(string file)
+  private static Dictionary<ulong, List<ulong>> Part1(List<ulong> seeds, Dictionary<string, List<List<ulong>>> almanac)
   {
-    string[] input = File.ReadAllLines(file);
-    List<ulong> seeds = input[0].Split(": ")[1].Split(" ").Select(ulong.Parse).ToList();
-    Dictionary<string, List<List<ulong>>> almanac = [];
-    string source = "";
-
-    foreach (string line in input[2..])
-    {
-      if (line == "")
-      {
-        continue;
-      }
-
-      if (line.Contains(':'))
-      {
-        source = line.Replace(" map:", "");
-        almanac[source] = [];
-      }
-      else
-      {
-        almanac[source].Add(line.Split(" ").Select(ulong.Parse).ToList());
-      }
-    }
-
     Dictionary<ulong, List<ulong>> correspondingNumbers = [];
 
     foreach (ulong seed in seeds)
@@ -55,10 +32,83 @@ internal class Program
       }
     }
 
-    // TODO: Part 2
+    return correspondingNumbers;
+  }
 
-    Console.WriteLine($"1: {correspondingNumbers.Min(kvp => kvp.Value.Last())}");
-    Console.WriteLine($"2: {2}");
+  // Brute force
+  private static ulong Part2(List<ulong> seeds, Dictionary<string, List<List<ulong>>> almanac)
+  {
+    var parallelOptions = new ParallelOptions
+    {
+      MaxDegreeOfParallelism = Environment.ProcessorCount
+    };
+
+    ulong result = ulong.MaxValue;
+
+    Parallel.For(0, seeds.Count, parallelOptions, i =>
+    {
+      if (i % 2 == 0)
+      {
+        ulong start = seeds[i];
+        ulong end = seeds[i + 1];
+
+        for (ulong seed = start; seed < start + end; seed++)
+        {
+          ulong number = seed;
+
+          foreach (var kvp in almanac)
+          {
+            foreach (List<ulong> numbers in kvp.Value)
+            {
+              ulong destRangeStart = numbers[0];
+              ulong srcRangeStart = numbers[1];
+              ulong rangeLength = numbers[2];
+
+              if (srcRangeStart <= number && number < srcRangeStart + rangeLength)
+              {
+                number = destRangeStart + number - srcRangeStart;
+                break;
+              }
+            }
+          }
+
+          result = Math.Min(result, Math.Min(seed, number));
+        }
+      }
+    });
+
+    return result;
+  }
+
+  public static void Solve(string file)
+  {
+    string[] input = File.ReadAllLines(file);
+    List<ulong> seeds = input[0].Split(": ")[1].Split(" ").Select(ulong.Parse).ToList();
+    Dictionary<string, List<List<ulong>>> almanac = [];
+    string source = "";
+
+    foreach (string line in input[2..])
+    {
+      if (line == "")
+      {
+        continue;
+      }
+
+      if (line.Contains(':'))
+      {
+        source = line.Replace(" map:", "");
+        almanac[source] = [];
+      }
+      else
+      {
+        almanac[source].Add(line.Split(" ").Select(ulong.Parse).ToList());
+      }
+    }
+
+    Dictionary<ulong, List<ulong>> part1 = Part1(seeds, almanac);
+
+    Console.WriteLine($"1: {part1.Min(kvp => kvp.Value.Last())}");
+    Console.WriteLine($"2: {Part2(seeds, almanac)}");
   }
 
   public static void Main()
