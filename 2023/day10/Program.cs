071b63985cf48e4648a19627b2108acbf33d412c
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2024-01-14 16:43:34 +0100

    added day 10 part 2 + refactor

--- 2023/day10/Program.cs
+++ 2023/day10/Program.cs
@@ -4,26 +4,27 @@ namespace Day10;
 
 internal class Program
 {
-  public static class Directions
+  private static char[,] grid = new char[0, 0];
+  private static Dictionary<(int x, int y), int> loop = [];
+  private static (int x, int y) startingPosition = (0, 0);
+
+  private static class Directions
   {
     public const string North = "North";
     public const string South = "South";
     public const string East = "East";
     public const string West = "West";
+
+    public static readonly List<(int dx, int dy, string dir)> All =
+    [
+      (-1, 0, North),
+      (1, 0, South),
+      (0, 1, East),
+      (0, -1, West)
+    ];
   }
 
-  /*
-  | is a vertical pipe connecting north and south
-  - is a horizontal pipe connecting east and west
-  L is a 90 - degree bend connecting north and east
-  J is a 90 - degree bend connecting north and west
-  7 is a 90 - degree bend connecting south and west
-  F is a 90 - degree bend connecting south and east
-  . is ground; there is no pipe in this tile
-  S is the starting position
-  */
-
-  public static Dictionary<char, Dictionary<string, List<char>>> pipeConnections = new()
+  private static readonly Dictionary<char, Dictionary<string, List<char>>> PipeConnections = new()
   {
     { '|', new Dictionary<string, List<char>> {
       { Directions.North, [ '|', '7', 'F' ] },
@@ -69,78 +70,120 @@ internal class Program
     }}
   };
 
-  public static (char[,] grid, (int, int) startPositon) ParseInput(string[] input)
+  private static void ParseInput(string[] input)
   {
     int rows = input.Length;
     int cols = input[0].Length;
-    char[,] grid = new char[rows, cols];
-    (int x, int y) startPositon = (0, 0);
+    grid = new char[rows, cols];
 
-    for (int i = 0; i < rows; i++)
+    for (int x = 0; x < rows; x++)
     {
-      for (int j = 0; j < cols; j++)
+      for (int y = 0; y < cols; y++)
       {
-        char c = input[i][j];
+        char c = input[x][y];
 
         if (c == 'S')
         {
-          startPositon = (i, j);
+          startingPosition = (x, y);
         }
 
-        grid[i, j] = c;
+        grid[x, y] = c;
       }
     }
-
-    return (grid, startPositon);
   }
 
-  private static void TraverseGrid(char[,] grid, (int x, int y) pos, Dictionary<(int x, int y), int> distances)
+  private static void TraverseLoop()
   {
     Queue<(int x, int y, int steps)> queue = [];
-    queue.Enqueue((pos.x, pos.y, 0));
+    loop = [];
+
+    queue.Enqueue((startingPosition.x, startingPosition.y, 0));
 
     while (queue.Count > 0)
     {
       var (x, y, steps) = queue.Dequeue();
-      // Console.WriteLine($"({x}, {y} - {steps})");
 
-      if (distances.ContainsKey((x, y)))
+      if (loop.ContainsKey((x, y)))
       {
         continue;
       }
 
-      distances.TryAdd((x, y), steps);
+      loop.TryAdd((x, y), steps);
 
-      if (x - 1 >= 0 && IsValidMove(grid[x, y], grid[x - 1, y], Directions.North))
+      foreach (var (dx, dy, direction) in Directions.All)
       {
-        queue.Enqueue((x - 1, y, steps + 1));
-      }
+        int nx = x + dx;
+        int ny = y + dy;
 
-      if (x + 1 < grid.GetLength(0) && IsValidMove(grid[x, y], grid[x + 1, y], Directions.South))
-      {
-        queue.Enqueue((x + 1, y, steps + 1));
-      }
-
-      if (y + 1 < grid.GetLength(1) && IsValidMove(grid[x, y], grid[x, y + 1], Directions.East))
-      {
-        queue.Enqueue((x, y + 1, steps + 1));
-      }
-
-      if (y - 1 >= 0 && IsValidMove(grid[x, y], grid[x, y - 1], Directions.West))
-      {
-        queue.Enqueue((x, y - 1, steps + 1));
+        if (nx >= 0 && nx < grid.GetLength(0) && ny >= 0 && ny < grid.GetLength(1))
+        {
+          if (PipeConnections[grid[x, y]][direction].Contains(grid[nx, ny]))
+          {
+            queue.Enqueue((nx, ny, steps + 1));
+          }
+        }
       }
     }
   }
 
-  private static bool IsValidMove(char fromPipe, char toPipe, string direction)
+  private static void RemoveUnconnectedPipes()
   {
-    bool isValid = pipeConnections[fromPipe][direction].Contains(toPipe);
-    // Console.WriteLine($"{fromPipe} -> {toPipe} ({direction}): {isValid}");
-    return isValid;
+    for (int x = 0; x < grid.GetLength(0); x++)
+    {
+      for (int y = 0; y < grid.GetLength(1); y++)
+      {
+        if (!loop.ContainsKey((x, y)))
+        {
+          grid[x, y] = '.';
+        }
+      }
+    }
   }
 
-  private static void PrintGrid(char[,] grid, Dictionary<(int x, int y), int>? distances = null)
+  private static int GetEnclosedTiles()
+  {
+    int count = 0;
+
+    for (int x = 0; x < grid.GetLength(0); x++)
+    {
+      for (int y = 0; y < grid.GetLength(1); y++)
+      {
+        if (grid[x, y] == '.')
+        {
+          if (IsEnclosed(x, y))
+          {
+            count++;
+          }
+        }
+      }
+    }
+
+    return count;
+  }
+
+  /* 
+  raycast from tile to the right. if it intersects with an odd number of pipes, it's enclosed
+  https://en.wikipedia.org/wiki/Point_in_polygon#Ray_casting_algorithm
+  ray cast up or down: ['-', 'L', 'F']
+  ray cast left or right: ['|', 'L', 'J']
+  */
+  private static bool IsEnclosed(int x, int startY)
+  {
+    int intersections = 0;
+
+    for (int y = startY; y < grid.GetLength(1); y++)
+    {
+      // unsure why this works but not: ['|', '7', 'F']
+      if (grid[x, y] == '|' || grid[x, y] == 'L' || grid[x, y] == 'J')
+      {
+        intersections++;
+      }
+    }
+
+    return intersections % 2 == 1;
+  }
+
+  private static void PrintGrid(bool showSteps = false)
   {
     Dictionary<char, char> unicodeBoxDrawing = new()
     {
@@ -154,17 +197,17 @@ internal class Program
       { 'S', 'S' },
     };
 
-    for (int i = 0; i < grid.GetLength(0); i++)
+    for (int x = 0; x < grid.GetLength(0); x++)
     {
-      for (int j = 0; j < grid.GetLength(1); j++)
+      for (int y = 0; y < grid.GetLength(1); y++)
       {
-        if (distances != null && distances.ContainsKey((i, j)))
+        if (showSteps && loop.ContainsKey((x, y)))
         {
-          Console.Write(distances[(i, j)]);
+          Console.Write(loop[(x, y)]);
         }
         else
         {
-          Console.Write(unicodeBoxDrawing[grid[i, j]]);
+          Console.Write(unicodeBoxDrawing[grid[x, y]]);
         }
       }
 
@@ -175,28 +218,27 @@ internal class Program
   public static void Solve(string file)
   {
     string[] input = File.ReadAllLines(file);
-    var (grid, startingPosition) = ParseInput(input);
-    Dictionary<(int x, int y), int> distances = [];
 
-    TraverseGrid(grid, startingPosition, distances);
-    PrintGrid(grid);
+    ParseInput(input);
+    TraverseLoop();
+    RemoveUnconnectedPipes();
+    // PrintGrid();
 
-    Console.WriteLine($"1: {distances.Values.Max()}");
+    Console.WriteLine($"1: {loop.Count / 2}");
+    Console.WriteLine($"2: {GetEnclosedTiles()}");
   }
 
   public static void Main()
   {
-    Stopwatch stopwatch = Stopwatch.StartNew();
+    Console.WriteLine("Day 10 - Advent of Code 2023");
+    Console.WriteLine("https://adventofcode.com/2023/day/10");
+    Console.WriteLine("********* Example ********");
 
+    Stopwatch stopwatch = Stopwatch.StartNew();
     string[] examples =
     [
       "example1.txt", "example2.txt", "example3.txt", "example4.txt", "example5.txt"
     ];
-    string input = "input.txt";
-
-    Console.WriteLine("Day 10 - Advent of Code 2023");
-    Console.WriteLine("https://adventofcode.com/2023/day/10");
-    Console.WriteLine("********* Example ********");
 
     foreach (string example in examples)
     {
@@ -208,7 +250,7 @@ internal class Program
     Console.WriteLine("********* Puzzle *********");
 
     stopwatch.Restart();
-    Solve(input);
+    Solve("input.txt");
     Console.WriteLine($"({stopwatch.ElapsedMilliseconds} ms)");
   }
 }
